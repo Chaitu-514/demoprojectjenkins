394d5aaeea3fa8fe0565880ca5ec4407769be9db
Chaitu Vinnu <Chaitu Vinnu@CHAITUVINI>
2023-01-10 14:03:37 +0530

    demo jenkins commit

---
+++ JenkinsProject/src/main/java/Demo/DemoTest1.java
@@ -0,0 +1,18 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	
+	@Test
+	
+	public void m1()
+	{
+		Reporter.log("hello",true);
+	}
+
+}
---
+++ JenkinsProject/src/main/java/Demo/DemoTest2.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	
+	@Test
+	
+	public void m2()
+	{
+		Reporter.log("m2 running",true);
+	}
+
+}
