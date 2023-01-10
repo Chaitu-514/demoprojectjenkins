641c37a4f9b45ad3b6852cde414befec74608d8b
Chaitu Vinnu <Chaitu Vinnu@CHAITUVINI>
2023-01-10 12:32:37 +0530

    Jenkins Fourth commit

---
+++ Jenkins/src/main/java/JenkinsDemo/DemoTest1.java
@@ -0,0 +1,17 @@
+package JenkinsDemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	
+	@Test
+	
+	public void m1()
+	{
+		Reporter.log("hello",true);
+	}
+
+}
